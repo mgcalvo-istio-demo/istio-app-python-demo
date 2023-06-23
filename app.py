2cd32f37e2f19d106d4e677810844682f9b48fe9
Matias Gonzalo Calvo <matias.calvo@ripio.com>
2023-06-23 12:02:00 -0300

    chore: add two version to app.py

--- app.py
+++ app.py
@@ -10,7 +10,26 @@ app = Flask(__name__)
 
 @app.route('/api')
 def process_sleep():
+    start_time = time.time() 
+
     sleep_value = read_sleep_value()
+
+    if sleep_value is None:
+        return jsonify({'error': 'El archivo no existe'}), 500
+    
+    if not sleep_value.isdigit():
+        return jsonify({'error': 'El valor no es numérico'}), 500
+    
+    sleep_value = int(sleep_value)
+    time.sleep(sleep_value)
+
+    salida = callSimple(sleep_value, start_time)
+    #salida = callUrls(sleep_value, start_time)
+    
+    return salida
+    
+
+def callUrls(sleep_value, start_time):
     urls = [
         "http://www.google.com",
         "http://www.yahoo.com",
@@ -25,15 +44,6 @@ def process_sleep():
     ]
     url = random.choice(urls)
     response = requests.get(url)
-
-    if sleep_value is None:
-        return jsonify({'error': 'El archivo no existe'}), 500
-    
-    if not sleep_value.isdigit():
-        return jsonify({'error': 'El valor no es numérico'}), 500
-    
-    sleep_value = int(sleep_value)
-    time.sleep(sleep_value)
     
     # Obtener el hostname
     hostname = socket.gethostname()
@@ -41,8 +51,30 @@ def process_sleep():
     version = os.environ.get('VERSION', 'undefined')  # Obtener el valor de la variable de entorno "version" o asignar 'undefined' si no existe
 
     fecha_actual = datetime.datetime.now()
+
+    end_time = time.time()
+
+    processing_time = end_time - start_time
+    processing_time = round(processing_time, 3)
     
-    return make_response(jsonify({'message': f'Holis, dormi {sleep_value} segundos', 'version': version, 'hostname': hostname, 'fecha_actual': fecha_actual, 'url': url, 'status_code': response.status_code}), response.status_code)
+    return make_response(jsonify({'message': f'Holis, dormi {sleep_value} segundos', 'version': version, 'hostname': hostname, 'fecha_actual': fecha_actual, 'url': url, 'status_code': response.status_code, 'processing_time': processing_time} ), response.status_code)
+
+
+def callSimple(sleep_value, start_time):
+    # Obtener el hostname
+    hostname = socket.gethostname()
+
+    version = os.environ.get('VERSION', 'undefined')  # Obtener el valor de la variable de entorno "version" o asignar 'undefined' si no existe
+
+    fecha_actual = datetime.datetime.now()
+
+    end_time = time.time()
+
+    processing_time = end_time - start_time
+    processing_time = round(processing_time, 3)
+    
+    return make_response(jsonify({'message': f'Holis, dormi {sleep_value} segundos', 'version': version, 'hostname': hostname, 'fecha_actual': fecha_actual, 'processing_time': processing_time}), 200)
+
 
 def read_sleep_value():
     sleep_file = '/tmp/sleep'
